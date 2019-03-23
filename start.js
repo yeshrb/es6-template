ebeca916bce2ae3bada70607b637fc3ab88c532c
= <=>
2019-03-23 09:55:33 +0800

    完成babel配置，可用babel7.0运行js

---
+++ start.js
@@ -0,0 +1,20 @@
+//作为备用参考，暂无用处
+
+require("@babel/register")({
+  presets: ["@babel/env"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./"],
+        alias: {
+          "~": "./server/",
+          database: "./server/database/"
+        }
+      }
+    ]
+  ]
+});
+
+require("@babel/polyfill");
+require("./server");
