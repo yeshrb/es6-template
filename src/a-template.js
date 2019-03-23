5adb7ddef156172b7850587092c1cc92db370d62
= <=>
2019-03-23 16:45:56 +0800

    修改是构建通过

--- src/a-template.js
+++ src/a-template.js
@@ -1,7 +1,8 @@
 //为了测试babel是否起作用
 //用yarn run ex src/a-template.js运行，运行通过，打印出此文件的路径即为验证成功
-import fs from 'path';
-const fn = ()=>{
+const fs = require("path");
+const fn = function() {
   console.log(fs.resolve(__dirname,__filename));
 };
-export default fn;
+
+module.exports = fn;
