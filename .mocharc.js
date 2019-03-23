7c11ec2fd8e05d20821515bf3b08d034812bc921
= <=>
2019-03-23 10:08:49 +0800

    完成mocha+chai的配置

---
+++ .mocharc.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Here's a JavaScript-based config file.
+// If you need conditional logic, you might want to use this type of config.
+// Otherwise, JSON or YAML is recommended.
+
+module.exports = {
+  diff: true,
+  extension: ["js"],
+  // opts: './test/mocha.opts',
+  // package: './package.json',
+  reporter: "spec",
+  slow: 75,
+  timeout: 2000,
+  ui: "bdd",
+  recursive: [
+    "./test/**/*.spec.js"
+  ],
+  require: ["@babel/polyfill", "@babel/register"]
+
+
+};
+
+
---
+++ test/testMochaCanRun.spec.js
@@ -0,0 +1,7 @@
+import chai from 'chai';
+const expect = chai.expect;
+describe("mocha+chai can run",()=> {
+  it("should success",()=>{
+    expect(1).to.equal(1);
+  })
+});
